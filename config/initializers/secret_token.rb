c9e5cbd7cfae7401412eacb0d1201e6b51f9cc98
brownie3003 <brownie3003@gmail.com>
2013-04-27 14:00:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirNights::Application.config.secret_key_base = 'ec6d75bfac3d02334e4ce868d9484af1a9e5890d0f04965439f00a4384fcc4d2531c51240a735be821443aef0f37b2cd41eb95615bb8a5ac7481ca136789bd87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AirNights::Application.config.session_store :encrypted_cookie_store, key: '_AirNights_session'
